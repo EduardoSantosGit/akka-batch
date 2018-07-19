e715370be548b45cf4ff6004b4d3b6347be6e189
eduardosantosgit <eduardosantos058@gmail.com>
2018-07-19 20:12:57 -0300

    add akka net project init actors

--- Akka-Batch/Program.cs
+++ Akka-Batch/Program.cs
@@ -1,13 +1,36 @@
-﻿using System;
+﻿using Akka.Actor;
+using Akka.Configuration;
+using System;
 
 namespace Akka_Batch
 {
     class Program
     {
+        public static ActorSystem SystemStart;
+
         static void Main(string[] args)
         {
 
+            var akkaConfig = ConfigurationFactory.ParseString(@"
+                akka {  
+                    actor {
+                        provider = ""Akka.Remote.RemoteActorRefProvider, Akka.Remote""
+                    }
+                    remote {
+                        dot-netty.tcp {
+                            port = 8081
+                            hostname = 0.0.0.0
+                            public-hostname = localhost
+                        }
+                    }
+                }
+                ");
 
+
+            SystemStart = ActorSystem.Create("SystemStart", akkaConfig);
+            //var messages = SystemStart.ActorOf(Props.Create(() => new ReceiverMessageActor()), ActorPath.Receiver.Name);
+            //messages.Tell("start");
+            SystemStart.WhenTerminated.Wait();
 
         }
     }
