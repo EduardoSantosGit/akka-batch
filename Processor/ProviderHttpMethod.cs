1c4a039654c9525bf19110e07d4270bed019612a
eduardosantosgit <eduardosantos058@gmail.com>
2018-09-12 13:08:27 -0300

    add basic get and client init

---
+++ Processor/ProviderHttpMethod.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Processor
+{
+    public class ProviderHttpMethod
+    {
+        private readonly HttpClient _client;
+
+        public ProviderHttpMethod(HttpClient client)
+        {
+            _client = client;
+        }
+
+        public async Task<HttpResponseMessage> GetAsync(string url)
+        {
+            var response = await _client.GetAsync(url);
+
+            return response;
+        }
+
+    }
+}
--- Processor/RequestClient.cs
+++ Processor/RequestClient.cs
@@ -5,5 +5,16 @@ namespace Processor
     public class RequestClient
     {
 
+        public Result<string> GetDataApi(string url)
+        {
+            if (string.IsNullOrEmpty(url))
+                return new Result<string>(ResultCode.BadRequest, "Param is null or empty");
+
+            
+
+
+            return new Result<string>(ResultCode.OK);
+        }
+
     }
 }
