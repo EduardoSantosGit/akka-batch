061c01d166ac108e51b702c4b14003af231ed9de
EduardoSantosGit <eduardosantos058@gmail.com>
2018-10-11 12:00:00 +0100

    add configs dataflow class

---
+++ Processor/PipeLine.cs
@@ -0,0 +1,35 @@
+﻿using Gridsum.DataflowEx;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace Processor
+{
+    public class PipeLine : Dataflow<string>
+    {
+        private ExecutionDataflowBlockOptions _options;
+        public PipeLine(
+            DataflowOptions dataflowOptions,
+            int maxDegreeOfParallelism) 
+            : base(dataflowOptions)
+        {
+            _options = dataflowOptions.ToExecutionBlockOption(true);
+            _options.MaxDegreeOfParallelism = maxDegreeOfParallelism;
+            _options.BoundedCapacity = 10000;
+            _options.MaxMessagesPerTask = 100;
+            _options.SingleProducerConstrained = true;
+        }
+
+        public void BatchRequest(List<string> items)
+        {
+
+
+
+        }  
+
+
+        public override ITargetBlock<string> InputBlock => throw new NotImplementedException();
+    }
+}
