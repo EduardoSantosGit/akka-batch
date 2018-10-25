e4b4cfae71c2fc679c2c4c2a1abf1bfa03a830c4
eduardosantosgit <eduardosantos058@gmail.com>
2018-10-25 15:00:00 +0100

    add v2 commander actor and ref

---
+++ Akka-Batch/Actors/v2/CommanderActor.cs
@@ -0,0 +1,30 @@
+﻿using Akka.Actor;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Akka.Batch.Actors.v2
+{
+    public class CommanderActor : ReceiveActor
+    {
+
+        public CommanderActor()
+        {
+
+        }
+
+        public void Receiving()
+        {
+
+        }    
+
+            
+
+        protected override void PreStart()
+        {
+            base.PreStart();
+        }
+
+
+    }
+}
--- Akka-Batch/Actors/v2/ProviderActor.cs
+++ Akka-Batch/Actors/v2/ProviderActor.cs
@@ -7,6 +7,9 @@ namespace Akka.Batch.Actors.v2
 {
     public class ProviderActor : ReceiveActor
     {
+
+        private IActorRef _commanderActor;
+
         public ProviderActor()
         {
             Starting();
@@ -28,6 +31,9 @@ namespace Akka.Batch.Actors.v2
 
         protected override void PreStart()
         {
+            _commanderActor = Context.ActorOf(Props.Create(() =>
+                new CommanderActor()), ActorPath.Commander.Name);
+
             base.PreStart();
         }
 
