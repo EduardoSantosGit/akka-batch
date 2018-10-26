4cb9439a58f75710a031d64a71734e82d748129a
eduardosantosgit <eduardosantos058@gmail.com>
2018-10-26 15:00:00 +0100

    create flow basic actors

---
+++ Akka-Batch/Actors/v2/CoordinatorActor.cs
@@ -0,0 +1,22 @@
+﻿using Akka.Actor;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Akka.Batch.Actors.v2
+{
+    public class CoordinatorActor : ReceiveActor
+    {
+
+        public CoordinatorActor()
+        {
+
+        }
+
+        protected override void PreStart()
+        {
+            base.PreStart();
+        }
+
+    }
+}
---
+++ Akka-Batch/Actors/v2/WorkerActor.cs
@@ -0,0 +1,21 @@
+﻿using Akka.Actor;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Akka.Batch.Actors.v2
+{
+    public class WorkerActor : ReceiveActor
+    {
+        public WorkerActor()
+        {
+
+        }
+
+        protected override void PreStart()
+        {
+            base.PreStart();
+        }
+
+    }
+}
